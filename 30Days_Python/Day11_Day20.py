000bb2f77b8a2d331a38535763c472d3301312f1
abc <abc@gmail.com>
2025-10-26 15:55:47 +0700

    PY12

--- 30Days_Python/Day11_Day20.py
+++ 30Days_Python/Day11_Day20.py
@@ -10,3 +10,16 @@
 
 
 # Day12
+
+# p,q,r=10,20,30          x,y=10,20           if 100 != 1000:
+# print(p,q,r)            print(x < y)           print('OK')
+#                                             if 100 == 1000:
+# A . Error              A . 20                  print('OKOK')
+# B . 10, 20, 30         B . True
+# C . 10, 20             C . 10               A . Error
+# D . 20, 30             D . False            B . OK
+#                                             C . OKOK
+#                                             D . OKOKOK
+
+# Day13
+
