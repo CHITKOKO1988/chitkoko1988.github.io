e76a3aa4fb7b13e198c6eacf20d02d87204fc91d
abc <abc@gmail.com>
2025-10-26 14:22:24 +0700

    PY

--- 30Days_Python/helloworld.py
+++ 30Days_Python/helloworld.py
@@ -1,1 +1,33 @@
-print("Hello World")
+# Day1/ Day2
+
+# name=input("Enter your name:")
+# age=input("Enter your age:")
+# print("My name is:",name)
+# print("My Age is:",age)
+
+# Day2-2
+
+# num1=int(input("Enter First Number:"))
+# num2=int(input("Enter Second Number:"))
+# num3=num1/num2
+# print("Answer:",num3)
+
+# Day3-1
+
+# ver=10            # interger variable
+# ver1="hello"      # string variable
+# ver2=2.5          # float variable
+# ver3= True         # boot variable
+# ver4= False
+# print(type(ver))
+# print(type(ver1))
+# print(type(ver2))
+# print(type(ver3))
+# print(type(ver4))
+
+# Day3-2
+
+pi=3.14
+radius=float(input('Enter your Circle:'))
+area=pi * radius * radius
+print('Area of Circle:',area)
